e1cd6b6ef5e075385fc0e9c2944e577b222bc8d2
AuHomie <143470710+AuHomie@users.noreply.github.com>
2025-09-10 11:08:50 -0600

    Update Graph.h
    video update.

--- src/Graph.h
+++ src/Graph.h
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <list>
+#include <string>
+
+struct Pair {
+
+int node;
+int weight;
+};
+
+class graph{
+
+    Graph(string);
+    int shortesdtCost(int start, int finish);
+private:
+    list<Pair>* adjList;
+};
+
 
